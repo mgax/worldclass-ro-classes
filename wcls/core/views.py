d27e364557a0b32ecb06c01bd1b8189f51ebc08a
Alex Morega <alex@grep.ro>
2023-07-01 16:46:01 +0300

    Show filtered classes on homepage

--- wcls/core/views.py
+++ wcls/core/views.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Class
+
+
+def homepage(request):
+    context = dict(
+        classes=Class.objects.filter(
+            title__in=settings.WCLS_CLASSES,
+        ).order_by("day", "hours"),
+    )
+    return render(request, "homepage.html", context)
